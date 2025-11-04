170656743e413a23f111d557eb9eb52bbd45ceb2
duclacoder <ducpvse818343@fpt.edu.vn>
2025-11-04 21:57:25 +0700

    Refund

---
+++ EV.Domain/Models/UserPackage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EV.Domain.Models;
+
+public partial class UserPackage
+{
+    public int UserPackagesId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? PackageId { get; set; }
+
+    public int? PaymentsId { get; set; }
+
+    public int? PurchasedPostDuration { get; set; }
+
+    public decimal? PurchasedAtPrice { get; set; }
+
+    public string? Currency { get; set; }
+
+    public DateTime? PurchasedAt { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual PostPackage? Package { get; set; }
+
+    public virtual Payment? Payments { get; set; }
+
+    public virtual User? User { get; set; }
+
+    public virtual ICollection<UserPost> UserPosts { get; set; } = new List<UserPost>();
+}
